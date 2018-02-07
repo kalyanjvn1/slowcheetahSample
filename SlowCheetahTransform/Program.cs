6abe3fbedeaa75e97609dd38c77fbc315466dbf7
kalyanjvn <kalyanjvn@gmail.com>
2018-02-06 21:37:50 -0600

    Create Test environment specific configuration manager and use slow cheetah to transform the config files

--- SlowCheetahTransform/Program.cs
+++ SlowCheetahTransform/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace SlowCheetahTransform
     {
         static void Main(string[] args)
         {
+           System.Collections.Specialized.NameValueCollection settings = ConfigurationManager.AppSettings;
+
+            //foreach (ConnectionStringSettings cs in settings)
+            //{
+            //    Console.WriteLine(cs.Name);
+            //    Console.WriteLine(cs.ConnectionString);
+            //}
+
+            foreach (var appSetting in settings)
+            {
+               Console.WriteLine(settings[appSetting.ToString()]);     
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
